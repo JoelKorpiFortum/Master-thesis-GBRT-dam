090ed9d2b6faabf3c236f296bbea95c604b842ab
Joel Korpi <joel.korpi@partners.fortum.com>
2025-03-25 16:10:35 +0100

    Changed hyperparameter tuning to Bayesian with Optuna instead of GridsearchCV

--- models/LightGBM.py
+++ models/LightGBM.py
@@ -2,19 +2,24 @@
 
 import numpy as np
 import pandas as pd
-import lightgbm as lgb
+from lightgbm import LGBMRegressor
+import optuna
+from optuna.samplers import TPESampler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.inspection import permutation_importance
 from sklearn.model_selection import GridSearchCV, TimeSeriesSplit, cross_val_score
+from dateutil.relativedelta import relativedelta
 import time
 import pickle
 import sys
 from pathlib import Path
 root_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(root_dir))
-from utils.data_preparation import preprocess_data, split_data
+from utils.data_preparation import preprocess_data, split_data, mapping
 from processing.custom_metrics import willmotts_d, nash_sutcliffe
 import multiprocessing
+from scipy.stats import pearsonr
+from tqdm import tqdm  # progress bar
 
 start_time = time.time()
 
@@ -24,15 +29,22 @@ features = ['GV1', 'GV3', 'GV51', 'MB4', 'MB8', 'MB10', 'MB18', 'h', \
             'h_MA_007', 'h_MA_014', 'h_RC_007', 'h_RC_030', 'P', 'P_RS_030', \
             'P_RS_060', 'P_RS_090', 'P_RS_180', 'T', 'T_MA_001', 'T_MA_007', \
             't', 'ln_t', 'Cos_s', 'Sin_s', 'Cos_2s', 'Sin_2s', 'month', 'year']
-
 target = 'MB4'
 features.remove(target)
+
 poly_degree = 4
-start_date = "08-01-2020"
-end_date = "03-01-2025"
+path = f'./data/LOS_DAMM_{mapping(target)}.csv'
+data = pd.read_csv(path, sep=';', parse_dates=['Date-Time'])
+
+dates = data['Date-Time']
+start_date = dates.iloc[0].date()  # First date (YYYY-MM-DD)
+end_date = dates.iloc[-1].date()  # Last date (YYYY-MM-DD)
+
 test_size = 0.3
-# split_index = 28107
-separation_date = "10-16-2023" # Date corresponds to split_index
+split_index = int(len(data) * (1 - test_size))
+separation_date = dates.iloc[split_index].date()
+difference = relativedelta(separation_date, start_date)
+total_months = difference.years * 12 + difference.months
 
 X, y, dates = preprocess_data(features, target, start_date, end_date, poly_degree=poly_degree, test_size=test_size)
 X_train, X_test, y_train, y_test, split_index = split_data(X, y, test_size=test_size)
@@ -40,45 +52,77 @@ X_all = pd.concat([X_train, X_test])
 
 tscv = TimeSeriesSplit(n_splits=3, test_size=int(0.1*len(X)))
 
-param_grid = {
-    'n_estimators': [500],     # Number of boosting rounds (equivalent to max_iter in your original code)
-    'learning_rate': [0.01],        # Step size at each iteration (your 'learning_rate')
-    'max_depth': [3],               # Maximum depth of the tree
-    'min_child_weight': [1],        # Minimum sum of instance weight (i.e., the minimum number of samples in a leaf node)
-    'subsample': [0.6],             # Fraction of samples to use for training each tree
-    'colsample_bytree': [0.8],      # Fraction of features to use for each tree
-    'reg_alpha': [3, 5],               # L1 regularization term
-    'reg_lambda': [5],         # L2 regularization term (similar to your 'l2_regularization')
-    'random_state': [42],           # random state for reproducibility
-    'boosting_type': ['gbdt'],      # gbdt or dart, dart takes longer time and works better for more boosting rounds
-    'linear_tree' : [True, False]
-}
+# 3-fold time series cross validation
+def timecv_model(model, X, y):
+    tfold = TimeSeriesSplit(n_splits=3, test_size=int(0.1*len(X)))
+    pcc_list = []
+    for _, (train_index, test_index) in tqdm(enumerate(tfold.split(X), start=1)):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        reg = model.fit(X_train, y_train)
+        pred = reg.predict(X_test)
+        pcc = pearsonr(pred, y_test) 
+        pcc_list.append(pcc[0])
+    
+    return pcc_list
 
-# Initialize the LGBMRegressor
-lgb_model = lgb.LGBMRegressor(n_jobs=-multiprocessing.cpu_count() // 2, verbose=-1) # linear_tree=True
+def cv_result(model, X, y):
+    model_name = model.__class__.__name__
+    pcc_ = timecv_model(model, X, y)
+    for i, pcc in enumerate(pcc_):
+        print(f'{i}th fold: {model_name} PCC: {pcc:.4f}')
+    print(f'\n{model_name} average PCC: {np.mean(pcc_):.4f}')
 
-# Set up the GridSearchCV with time series split and negative RMSE scoring
-grid_search = GridSearchCV(
-    estimator=lgb_model, #type: ignore
-    param_grid=param_grid, 
-    cv=tscv, 
-    scoring='neg_root_mean_squared_error', 
-    n_jobs=-multiprocessing.cpu_count() // 2,
-    verbose=1
-)
+# Uncomment to suppress optuna log messages
+# optuna.logging.set_verbosity(optuna.logging.WARNING)
 
-grid_search.fit(X_train, y_train)
-best_params = grid_search.best_params_
-model = grid_search.best_estimator_ 
+sampler = TPESampler(seed=42)
 
-cv_results = grid_search.cv_results_
-best_index = grid_search.best_index_
-rmse_cv = -cv_results['mean_test_score'][best_index]
+def objective_lgb(trial):
+    """
+    The objective function to tune hyperparameters. It evaluate the score on a
+    validation set. This function is used by Optuna, a Bayesian hyperparameter tuning framework.
+    """
+        
+    params = {
+            'objective': 'regression',
+            'verbose': -1,
+            'random_state': 42,
+            'max_depth': trial.suggest_int('max_depth', 3, 12),
+            'num_leaves': trial.suggest_int('num_leaves', 2, 30),
+            'learning_rate': trial.suggest_float("learning_rate", 1e-5, 0.1),
+            'n_estimators': trial.suggest_int('n_estimators', 500, 3000),
+            'subsample': trial.suggest_float('subsample', 0.5, 1),
+            'feature_fraction': trial.suggest_float('feature_fraction', 0.2, 1),
+            "min_gain_to_split": trial.suggest_float("min_gain_to_split", 0, 15),
+            'reg_alpha': trial.suggest_float('reg_alpha', 0, 5),               # L1 regularization term
+            'reg_lambda': trial.suggest_float('reg_lambda', 0, 5),             # L2 regularization term (similar to your 'l2_regularization')
+            # 'boosting_type': trial.suggest_categorical('boosting_type', ['gbdt', 'dart']),
+            'linear_tree': trial.suggest_categorical('linear_tree', [True, False])
+            }
+    model = LGBMRegressor(**params)
+    model.fit(X_train, y_train, eval_set = [(X_train, y_train), (X_test, y_test)])
+    pred = model.predict(X_test)
+    # pcc = pearsonr(pred, y_test)[0]
+    rmse = np.sqrt(mean_squared_error(pred, y_test))
+    return rmse
+
+study_model = optuna.create_study(direction = 'minimize', sampler = sampler, study_name='hyperparameters_tuning')
+study_model.optimize(objective_lgb, n_trials = 30)  # type: ignore
+
+trial = study_model.best_trial
+best_params = trial.params
+
+print('Best params from optuna: \n', best_params)
+
+opt_model = LGBMRegressor(**best_params)
+
+cv_result(opt_model, X, y)
 
 # Predictions
-train_predictions = model.predict(X_train) #type: ignore
-test_predictions = model.predict(X_test) #type: ignore
-all_predictions = model.predict(X_all) #type: ignore
+train_predictions = opt_model.predict(X_train) #type: ignore
+test_predictions = opt_model.predict(X_test) #type: ignore
+all_predictions = opt_model.predict(X_all) #type: ignore
 
 # Evaluation
 rmse_train = np.sqrt(mean_squared_error(y_train, train_predictions))
@@ -92,17 +136,17 @@ NSE_test = nash_sutcliffe(y_test, test_predictions)
 # test_score = np.zeros((model.n_iter_,), dtype=np.float64)
 # for i, y_pred in enumerate(model.staged_predict(X_test)):
 #     test_score[i] = mean_squared_error(y_test, y_pred)
-iterations = np.arange(model.n_iter_) + 1 #type: ignore
+# iterations = np.arange(model.n_iter_) + 1 #type: ignore
 
 # Compute permutation importance
 actual_feature_names = X.columns
-perm = permutation_importance(model, X_test, y_test, n_repeats=10, random_state=42, n_jobs=2)
+perm = permutation_importance(opt_model, X_test, y_test, n_repeats=10, random_state=42, n_jobs=2)
 perm_idx = perm.importances_mean.argsort() #type: ignore
 perm_importances = perm.importances[perm_idx].T #type: ignore
 perm_labels = actual_feature_names[perm_idx]
 
 
-fea_imp_ = pd.DataFrame({'cols':X_train.columns, 'fea_imp':model.feature_importances_}) #type: ignore
+fea_imp_ = pd.DataFrame({'cols':X_train.columns, 'fea_imp':opt_model.feature_importances_}) #type: ignore
 fea_labels = fea_imp_.loc[fea_imp_.fea_imp > 0].sort_values(by=['fea_imp'], ascending = False)
 
 # Prepare data for plotting
@@ -119,7 +163,7 @@ plotting_data = {
     'NSE': NSE_test,
 #     'train_score': train_score,
 #     'test_score': test_score,
-    'iterations': iterations,
+    # 'iterations': iterations,
     'perm_importances': perm_importances,
     'perm_labels':perm_labels,
     'native_importance': fea_imp_,
@@ -133,17 +177,70 @@ with open(f'./visualization/plotting_data/{model_type}/{model_type}_{target}_plo
     pickle.dump(plotting_data, f)
 
 with open(f'./visualization/models/{model_type}/{model_type}_model_{target}.pkl', 'wb') as file:
-    pickle.dump(model, file)
+    pickle.dump(opt_model, file)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 
-print(f"RMSE_CV: {rmse_cv}")
+print(f"\n~~~ MODEL ~~~ \n")
+print(f"Best parameters: {best_params}")
+print(f"\n~~~ TEST METRICS ~~~ \n")
+# print(f"RMSE_CV: {rmse_cv}")
 print(f"RMSE_train: {rmse_train}")
 print(f"RMSE_test: {rmse_test}")
 print(f"MAE_test: {mae_test}")
 print(f"Willmott's d Test: {d_test}")
 print(f"Nash-Sutcliffe Test: {NSE_test}")
-print(f"N iter: {model.n_iter_}") #type: ignore
-print(f"Best parameters: {best_params}")
-print(f"Elapsed time: {elapsed_time:.4f} seconds")
+print(f"\n~~~ OTHER STATS ~~~ \n")
+print(f"Train data length: {total_months} months")
+# print(f"N iter: {opt_model.n_iter_}") #type: ignore
+print(f"Elapsed time: {elapsed_time:.4f} seconds\n")
+
+
+# Initialize the LGBMRegressor
+# lgb_model = LGBMRegressor(n_jobs=-multiprocessing.cpu_count() // 2, verbose=-1) # linear_tree=True
+
+# # param_grid = {
+            # 'n_estimators': [500],          # Number of boosting rounds (equivalent to max_iter in your original code)
+            # 'learning_rate': [0.01],        # Step size at each iteration (your 'learning_rate')
+            # 'max_depth': [3],               # Maximum depth of the tree
+            # 'min_child_weight': [1],        # Minimum sum of instance weight (i.e., the minimum number of samples in a leaf node)
+            # 'subsample': [0.6],             # Fraction of samples to use for training each tree
+            # 'colsample_bytree': [0.8],      # Fraction of features to use for each tree
+            # 'reg_alpha': [3, 5],            # L1 regularization term
+            # 'reg_lambda': [5],              # L2 regularization term (similar to your 'l2_regularization')
+            # 'random_state': [42],           # random state for reproducibility
+            # 'boosting_type': ['gbdt'],      # gbdt or dart, dart takes longer time and works better for more boosting rounds
+            # 'linear_tree' : [True, False]
+            # }
+
+# # Set up the GridSearchCV with time series split and negative RMSE scoring
+# grid_search = GridSearchCV(
+#     estimator=lgb_model, #type: ignore
+#     param_grid=param_grid, 
+#     cv=tscv, 
+#     scoring='neg_root_mean_squared_error', 
+#     n_jobs=-multiprocessing.cpu_count() // 2,
+#     verbose=1
+# )
+
+# grid_search.fit(X_train, y_train)
+# best_params = grid_search.best_params_
+# model = grid_search.best_estimator_ 
+
+# cv_results = grid_search.cv_results_
+# best_index = grid_search.best_index_
+# rmse_cv = -cv_results['mean_test_score'][best_index]
+
+# MB4 Optimal params
+# Best parameters: {'max_depth': 9, 
+#                   'num_leaves': 19, 
+#                   'learning_rate': 0.046407238709264644, 
+#                   'n_estimators': 1220, 
+#                   'colsample_bytree': 0.8312585492418, 
+#                   'subsample': 0.84147634768488, 
+#                   'feature_fraction': 0.9861489254222437, 
+#                   'min_gain_to_split': 9.479400587354975, 
+#                   'reg_alpha': 2.702546561764053, 
+#                   'reg_lambda': 0.27946343733889023, 
+#                   'linear_tree': False}
